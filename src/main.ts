a58c0a112e5dd569a682f4bd15c27c3b10990f5e
Khalil Aliev <khalilaliev@Khalil.local>
2024-06-01 14:40:20 +0200

    added zoom animation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import "./style.css";
-import MapInit from "./services/map/init.ts";
+import mapInit from "./services/map/init.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* html */ `
   <div id = "map" style = "overflow: hidden; width: 100%; height: 100vh;"></div>
 `;
 
-const map = new MapInit("map");
+const { map } = new mapInit("map");
 console.log(map);
+
+map.on("load", () => {
+  console.log(map.getStyle().layers);
+});
+map.on("click", (e) => {
+  console.log(e);
+});
--- src/services/map/init.ts
+++ src/services/map/init.ts
@@ -4,24 +4,31 @@ mapboxgl.accessToken =
   "pk.eyJ1Ijoia2hhbGlsYWxpZXYiLCJhIjoiY2x3ODBhdWR5MXp1MDJscGR0eGgxMXh2aSJ9.ppOwu4hFatGJKjiMFvoDZA";
 
 class MapInit {
-  private _map: Map;
+  private _map: mapboxgl.Map;
 
   constructor(idContainer: string) {
     this._map = this.setupMap(idContainer);
-  }
-  get map(): Map {
-    return this._map;
-  }
-  set map(value: Map) {
-    this._map = value;
+    this.flyToCoordinates([8.725689367209213, 47.50045980431281], 13);
   }
 
-  setupMap(container: string): Map {
+  get map(): mapboxgl.Map {
+    return this._map;
+  }
+
+  setupMap(container: string): mapboxgl.Map {
     return new mapboxgl.Map({
       container: container,
-      style: "mapbox://styles/mapbox/light-v11",
-      center: [-74.5, 40],
-      zoom: 9,
+      style: "mapbox://styles/mapbox/dark-v11",
+      center: [0, 0], // Установите временный центр карты
+      zoom: 1, // Установите временный зум
+    });
+  }
+
+  flyToCoordinates(coordinates: [number, number], zoom: number): void {
+    this._map.flyTo({
+      center: coordinates,
+      zoom: zoom,
+      essential: true, // Это гарантирует, что анимация будет выполнена, даже если вкладка не активна
     });
   }
 }
